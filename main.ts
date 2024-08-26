bd599a0649d626a6dc1391780945336211318561
Ryan <ryanisaacg@users.noreply.github.com>
2024-08-25 23:11:02 -0400

    Initial diary commit, hard-coded to my config

--- main.ts
+++ main.ts
@@ -7,148 +7,40 @@ import {
 	Plugin,
 	PluginSettingTab,
 	Setting,
+	WorkspaceLeaf,
 } from "obsidian";
-
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
-}
-
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: "default",
-};
+import { WeekReviewView, WEEK_REVIEW_TYPE } from "week-review-view";
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	override async onload() {
+		this.registerView(WEEK_REVIEW_TYPE, (leaf) => new WeekReviewView(leaf));
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon(
-			"dice",
-			"Sample Plugin",
-			(evt: MouseEvent) => {
-				// Called when the user clicks the icon.
-				new Notice("This is a notice!");
-			},
-		);
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass("my-plugin-ribbon-class");
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText("Status Bar Text");
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: "open-sample-modal-simple",
-			name: "Open sample modal (simple)",
-			callback: () => {
-				new SampleModal(this.app).open();
-			},
+		this.addRibbonIcon("dice", "Activate view", () => {
+			this.activateView();
 		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: "sample-editor-command",
-			name: "Sample editor command",
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection("Sample Editor Command");
-			},
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: "open-sample-modal-complex",
-			name: "Open sample modal (complex)",
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView =
-					this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, "click", (evt: MouseEvent) => {
-			console.log("click", evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log("setInterval"), 5 * 60 * 1000),
-		);
 	}
 
 	onunload() {}
 
-	async loadSettings() {
-		this.settings = Object.assign(
-			{},
-			DEFAULT_SETTINGS,
-			await this.loadData(),
-		);
-	}
+	async saveSettings() {}
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+	async activateView() {
+		const { workspace } = this.app;
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+		let leaf: WorkspaceLeaf | null = null;
+		const leaves = workspace.getLeavesOfType(WEEK_REVIEW_TYPE);
 
-	onOpen() {
-		const { contentEl } = this;
-		contentEl.setText("Woah!");
-	}
+		if (leaves.length > 0) {
+			// A leaf with our view already exists, use that
+			leaf = leaves[0];
+		} else {
+			// Our view could not be found in the workspace, create a new leaf
+			// in the right sidebar for it
+			leaf = workspace.getRightLeaf(false);
+			await leaf?.setViewState({ type: WEEK_REVIEW_TYPE, active: true });
+		}
 
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const { containerEl } = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName("Setting #1")
-			.setDesc("It's a secret")
-			.addText((text) =>
-				text
-					.setPlaceholder("Enter your secret")
-					.setValue(this.plugin.settings.mySetting)
-					.onChange(async (value) => {
-						this.plugin.settings.mySetting = value;
-						await this.plugin.saveSettings();
-					}),
-			);
+		// "Reveal" the leaf in case it is in a collapsed sidebar
+		workspace.revealLeaf(leaf!);
 	}
 }
---
+++ week-review-view.ts
@@ -0,0 +1,110 @@
+import {
+	ItemView,
+	MarkdownRenderer,
+	TAbstractFile,
+	TFile,
+	WorkspaceLeaf,
+} from "obsidian";
+
+export const WEEK_REVIEW_TYPE = "week-review-view";
+const WEEK_IN_MS = 7 * 24 * 60 * 60 * 1000;
+
+export class WeekReviewView extends ItemView {
+	root: Element;
+
+	constructor(leaf: WorkspaceLeaf) {
+		super(leaf);
+	}
+
+	override getViewType() {
+		return WEEK_REVIEW_TYPE;
+	}
+
+	override getDisplayText() {
+		return "Week Review";
+	}
+
+	override async onOpen() {
+		const container = this.containerEl.children[1];
+		this.root = container;
+		await this.render();
+		this.registerEvent(
+			this.app.vault.on("create", (file) => this.crudEvent(file)),
+		);
+		this.registerEvent(
+			this.app.vault.on("modify", (file) => this.crudEvent(file)),
+		);
+		this.registerEvent(
+			this.app.vault.on("delete", (file) => this.crudEvent(file)),
+		);
+		this.registerEvent(
+			this.app.vault.on("rename", (file) => this.crudEvent(file)),
+		);
+	}
+
+	crudEvent(file: TAbstractFile) {
+		const fileDate = pathToDate(file);
+		if (fileDate == null) {
+			return;
+		}
+		const diff = Date.now() - fileDate.getTime();
+		if (diff <= WEEK_IN_MS) {
+			this.render();
+		}
+	}
+
+	async render() {
+		this.root.empty();
+
+		const notes = this.getWeekNotes();
+		for (const entry of notes) {
+			this.root.createEl("h1", { text: entry.basename });
+			const contents = await this.app.vault.cachedRead(entry);
+			const container = this.root.createDiv();
+			MarkdownRenderer.render(
+				this.app,
+				contents,
+				container,
+				entry.path,
+				this,
+			);
+		}
+	}
+
+	override async onClose() {}
+
+	getWeekNotes(): TFile[] {
+		const entries: TFile[] = [];
+
+		for (let i = 0; i < 7; i += 1) {
+			const date = new Date();
+			date.setDate(date.getDate() - i);
+			const path = dateToPath(date);
+			const entry = this.app.vault.getFileByPath(path);
+			if (entry != null) {
+				entries.push(entry);
+			}
+		}
+
+		return entries;
+	}
+}
+
+function dateToPath(date: Date): string {
+	const year = date.getFullYear();
+	const month = String(date.getMonth() + 1).padStart(2, "0");
+	const dateStr = String(date.getDate()).padStart(2, "0");
+	return `diary/${year}-${month}-${dateStr}.md`;
+}
+
+function pathToDate(file: TAbstractFile): Date | null {
+	if (!file.name.endsWith("md")) {
+		return null;
+	}
+	const name = file.name.substring(0, file.name.length - ".md".length);
+	const [year, month, date] = name.split("-");
+	if (year == null || month == null || date == null) {
+		return null;
+	}
+	return new Date(Number(year), Number(month) - 1, Number(date));
+}
