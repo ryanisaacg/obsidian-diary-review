ea504a912e273e00729527827b169cf3cf8706d9
Ryan <ryanisaacg@users.noreply.github.com>
2024-08-25 23:17:42 -0400

    Allow creating more than one instance

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Plugin, WorkspaceLeaf } from "obsidian";
+import { Plugin } from "obsidian";
 import { WeekReviewView, WEEK_REVIEW_TYPE } from "week-review-view";
 
 export default class MyPlugin extends Plugin {
@@ -17,18 +17,8 @@ export default class MyPlugin extends Plugin {
 	async activateView() {
 		const { workspace } = this.app;
 
-		let leaf: WorkspaceLeaf | null = null;
-		const leaves = workspace.getLeavesOfType(WEEK_REVIEW_TYPE);
-
-		if (leaves.length > 0) {
-			// A leaf with our view already exists, use that
-			leaf = leaves[0];
-		} else {
-			// Our view could not be found in the workspace, create a new leaf
-			// in the right sidebar for it
-			leaf = workspace.getRightLeaf(false);
-			await leaf?.setViewState({ type: WEEK_REVIEW_TYPE, active: true });
-		}
+		const leaf = workspace.getRightLeaf(false);
+		await leaf?.setViewState({ type: WEEK_REVIEW_TYPE, active: true });
 
 		// "Reveal" the leaf in case it is in a collapsed sidebar
 		workspace.revealLeaf(leaf!);
